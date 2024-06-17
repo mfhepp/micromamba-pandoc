3562ff08719a47010a04914cede4121d23a3d312
mfhepp <mfhepp@users.noreply.github.com>
2024-06-18 01:44:19 +0200

    add micromamba stack

--- build-micromamba.sh
+++ build-micromamba.sh
@@ -11,12 +11,11 @@ usage ()
     printf '  -c: targeted pandoc commit, e.g. 2.9.2.1\n'
     printf '  -d: directory\n'
     printf '  -r: targeted image repository/flavor, e.g. core or latex\n'
-    printf '  -s: stack on which the image will be based\n'
     printf '  -t: docker build target\n'
     printf '  -v: increase verbosity\n'
 }
 
-if ! args=$(getopt 'c:d:pr:s:t:v' "$@"); then
+if ! args=$(getopt 'c:d:pr:t:v' "$@"); then
     usage && exit 1
 fi
 # The variable is intentionally left unquoted.
@@ -26,7 +25,7 @@ set -- $args
 directory=.
 pandoc_commit=main
 repo=core
-stack=static
+stack=micromamba
 target=${stack}-${repo}
 verbosity=0
 
@@ -42,10 +41,6 @@ while true; do
             ;;
         (-r)
             repo="${2}"
-            shift 2
-            ;;
-        (-s)
-            stack="${2}"
             shift 2
             ;;
         (-t)
@@ -77,68 +72,15 @@ if [ "$pandoc_commit" = "main" ]; then
     pandoc_version=edge
 fi
 
-# File containing the version table
-version_table_file="${directory}/versions.md"
-if [ ! -f "$version_table_file" ]; then
-    printf 'Version table not found: %s\n' "$version_table_file" >&2
-    exit 1
-fi
-# File containing the default stack config
-stack_table_file="${directory}/default-stack.md"
-if [ ! -f "$stack_table_file" ]; then
-    printf 'Stack table not found: %s\n' "$stack_table_file" >&2
-    exit 1
-fi
 
-pandoc_version_opts=$(grep "^| *${pandoc_commit} *|" "$version_table_file")
-if [ -z "$pandoc_version_opts" ]; then
-    printf 'Unsupported version: %s; aborting!\n' "$pandoc_commit" >&2
-    exit 1
-fi
-
-version_table_field ()
-{
-    printf '%s\n' "$pandoc_version_opts" | \
-        awk -F '|' "{ gsub(/^ *| *\$/,\"\",\$$1); print \$$1 }"
-}
-
-base_image_version=
-case "$stack" in
-    (alpine)
-        base_image_version=$(version_table_field 4)
-        ;;
-    (static)
-        # The static binary is built on alpine
-        base_image_version=$(version_table_field 4)
-        ;;
-    (ubuntu)
-        base_image_version=$(version_table_field 5)
-        ;;
-    (*)
-        printf 'Unknown stack: %s\n' "$stack" >&2
-        exit 1
-        ;;
-esac
-
-tag_versions=$(version_table_field 3)
-texlive_version=$(version_table_field 6)
-lua_version=$(version_table_field 7)
+base_image_version="latest"
+# tag_versions=$(version_table_field 3)
+texlive_version=2004
+lua_version=5.4
 
 # Crossref
 extra_packages=pandoc-crossref
 without_crossref=
-
-# Do not build pandoc-crossref for static images
-if [ "$stack" = "static" ]; then
-    extra_packages=
-    without_crossref=true
-fi
-
-## The pandoc-cli package did not exist pre pandoc 3.
-## Do not try to build it if the commit starts with a 2.
-if [ "${pandoc_commit#2}" = "${pandoc_commit}" ]; then
-    extra_packages="pandoc-cli ${extra_packages}"
-fi
 
 # Debug output
 if [ "$verbosity" -gt 0 ]; then
@@ -155,13 +97,6 @@ if [ "$verbosity" -gt 0 ]; then
     printf '\tversion_table_file: %s\n' "${version_table_file}"
 fi
 
-# Succeeds if the stack is the default for this repo, in which case the
-# stack can be omitted from the tag.
-is_default_stack_for_repo ()
-{
-    grep -q "^| *$repo *| *${stack} *|$" "$stack_table_file"
-}
-
 # ARG 1: pandoc version
 # ARG 2: stack
 image_name ()
@@ -171,27 +106,6 @@ image_name ()
     else
         printf 'pandoc/%s:%s-%s' "$repo" "${1:-edge}" "$2"
     fi
-}
-
-# List all tags for this image.
-tags ()
-{
-    for tag_version in $tag_versions; do
-        printf '%s\n' "$(image_name "$tag_version" "$stack")"
-    done
-    if is_default_stack_for_repo; then
-        for tag_version in $tag_versions; do
-            printf '%s\n' "$(image_name "$tag_version")"
-        done
-    fi
-}
-
-# Produce the "tag" command line arguments for `docker build`
-tag_arguments ()
-{
-    for tag in $(tags); do
-        printf ' --tag=%s' "$tag"
-    done
 }
 
 case "$action" in
